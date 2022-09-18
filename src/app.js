5730622c051796e94cc987fe001efceeebe7149e
Abrahamjf <abrahamjferrer@gmail.com>
2022-09-18 21:23:35 +0000

    :hammer: Se completo el proyecto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["The", "Our", "My", "A"];
+  let adjectives = ["great", "big", "small", "clever", "giant", "obnoxious"];
+  let nouns = ["cat", "grandma", "dog", "llama", "bird", "coworker"];
+
+  for (let i = 0; i < 1000; i++) {
+    const randomPronoun = () => {
+      return pronouns[Math.floor(Math.random() * pronouns.length)];
+    };
+
+    const randomAdj = () => {
+      return adjectives[Math.floor(Math.random() * adjectives.length)];
+    };
+
+    const randomNoun = () => {
+      return nouns[Math.floor(Math.random() * nouns.length)];
+    };
+
+    let nameDomain = randomPronoun() + randomAdj() + randomNoun() + ".com";
+
+    console.log(nameDomain);
+  }
 };
